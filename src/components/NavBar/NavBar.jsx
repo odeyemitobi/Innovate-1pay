2c1c5433600be13b3a0329c20cd53cdebee186ac
Odeyemi Tobiloba <odeyemioluwatobiloba11@gmail.com>
2023-05-08 18:36:22 +0100

    All libraries installed

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function NavBar() {
   return (
     <div>
-      <p className=" text-red-600">Hello</p>
+      <p className=" text-red-600 font-bold text-3xl">Hello</p>
     </div>
   );
 }
