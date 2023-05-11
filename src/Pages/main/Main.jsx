5076a7ab57662f2c8b4644890b11db902c2a5f9f
Odeyemi Tobiloba <odeyemioluwatobiloba11@gmail.com>
2023-05-11 10:01:37 +0100

    Ok

--- src/Pages/main/Main.jsx
+++ src/Pages/main/Main.jsx
@@ -6,7 +6,7 @@ import {
   MdOutlineTrendingDown,
   MdOutlineTrendingUp,
 } from "react-icons/md";
-import { BsArrowLeft, BsArrowRight } from "react-icons/bs"
+import { BsArrowLeft, BsArrowRight, BsCheck2Circle } from "react-icons/bs";
 import { FcComboChart } from "react-icons/fc";
 import { AiOutlineSafety } from "react-icons/ai";
 import Slidereact from "../sub/Slidereact";
@@ -14,16 +14,21 @@ import Contact from "../sub/Contact";
 import Custom from "../sub/Custom";
 import Real from "../sub/Real";
 import Pricing from "../sub/Pricing";
+import Bill from "../../Assets/img/bills.jpg";
+import Cbn from "../../Assets/img/Central.png";
+import First from "../../Assets/img/firstbank.png";
+import Lian from "../../Assets/img/Lian.jpg";
+import Swg from "../../Assets/img/Sw.jpg";
+import Gtb from "../../Assets/img/gtbank.jpg";
 
 function Main() {
-
   const scrollLeft = () => {
     document.getElementById("content").scrollLeft -= 600;
-  }
+  };
 
   const scrollRight = () => {
     document.getElementById("content").scrollLeft += 600;
-  }
+  };
 
   return (
     <MainLayout>
@@ -39,48 +44,6 @@ function Main() {
             <div className="pt-[8rem]">
               <StartedBtn>Get Started</StartedBtn>
             </div>
-            <div className="pt-[5rem]">
-              <div className="rounded-full cursor-pointer bg-blue-900 px-[40px] py-[40px]">
-                <div className="flex">
-                  <div className=" border-r-2 px-16">
-                    <p className="text-2xl leading-[3rem]">$USD</p>
-                    <div className="flex items-center">
-                      <p className="text-4xl font-semibold">430.00</p>
-                      <div className="pl-3">
-                        <MdOutlineTrendingUp size={35} color="#15a315" />
-                      </div>
-                    </div>
-                  </div>
-                  <div className=" border-r-2 px-16">
-                    <p className="text-2xl leading-[3rem]">£GBP</p>
-                    <div className="flex items-center">
-                      <p className="text-4xl font-semibold">430.00</p>
-                      <div className="pl-3">
-                        <MdOutlineTrendingDown size={35} color="red" />
-                      </div>
-                    </div>
-                  </div>
-                  <div className=" border-r-2 px-16">
-                    <p className="text-2xl leading-[3rem]">€EUR</p>
-                    <div className="flex items-center">
-                      <p className="text-4xl font-semibold">430.00</p>
-                      <div className="pl-3">
-                        <MdOutlineTrendingDown size={35} color="red" />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="px-16">
-                    <p className="text-2xl leading-[3rem]">¥ YUAN</p>
-                    <div className="flex items-center">
-                      <p className="text-4xl font-semibold">430.00</p>
-                      <div className="pl-3">
-                        <MdOutlineTrendingDown size={35} color="red" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
         <div className="pt-[9rem]">
@@ -93,7 +56,7 @@ function Main() {
             </div>
           </div>
         </div>
-        <div className="pt-[9rem] bg-[]">
+        <div className="pt-[9rem] bg-neutral-100">
           <div className="px-[18rem]">
             <div className="text-center">
               <h1 className="text-3xl font-semibold">OUR SERVICES ARE</h1>
@@ -133,14 +96,27 @@ function Main() {
         <div className="pt-[12rem] pb-[14rem] bg-[#f0f8ff]">
           <div className="px-[18rem]">
             <div className="text-center flex justify-between">
-              <BsArrowLeft className=" cursor-pointer" onClick={scrollLeft} size={40} color="black" />
+              <BsArrowLeft
+                className=" cursor-pointer"
+                onClick={scrollLeft}
+                size={40}
+                color="black"
+              />
               <h1 className="text-3xl font-semibold">
                 BENEFITS FOR YOUR BUSINESS
               </h1>
-              <BsArrowRight className=" cursor-pointer" onClick={scrollRight} size={40} color="black" />
+              <BsArrowRight
+                className=" cursor-pointer"
+                onClick={scrollRight}
+                size={40}
+                color="black"
+              />
             </div>
             <div className="pt-[9rem]">
-              <div id="content" className="w-full flex items-center justify-start overflow-x-auto scroll-smooth scrollbar-hide ">
+              <div
+                id="content"
+                className="w-full flex items-center justify-start overflow-x-auto scroll-smooth scrollbar-hide "
+              >
                 <div className="">
                   <Slidereact />
                 </div>
@@ -161,8 +137,130 @@ function Main() {
           </div>
         </div>
         <div className="map">
-          <div className="pt-5">
-            hello
+          <div className="py-[7rem] px-[28rem] flex justify-between">
+            <div className="w-[30%] text-left place-items-center grid">
+              <h1 className="text-[4rem]">
+                Holistic Foreign Exchange Solutions
+              </h1>
+            </div>
+            <div className="place-items-center grid">
+              <img
+                className="h-[45rem] w-[90%] rounded-[50%]"
+                src={Bill}
+                alt="Bill"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="w-full bg-neutral-200">
+          <div className="py-[9rem] px-[18rem]">
+            <div className="text-center">
+              <h1 className="text-3xl font-semibold">WHY TRUST US</h1>
+            </div>
+            <div className="pt-[8rem] flex justify-between">
+              <div className="w-full px-8">
+                <div className="w-[80%] border-b border-[#9B9B9B] pb-[3rem]">
+                  <div className="flex items-center">
+                    <BsCheck2Circle size={30} color="red" />
+                    <h1 className="pl-1 text-xl font-semibold">CBN Licensed</h1>
+                  </div>
+                  <div className="pt-3">
+                    <p className=" font-thin text-lg">
+                      Holistic foreign exchange solutions, including
+                      importation, exportation, and wholesale banknotes
+                      distribution.
+                    </p>
+                  </div>
+                </div>
+                <div className="w-[80%] border-b border-[#9B9B9B] py-[3rem]">
+                  <div className="flex items-center">
+                    <BsCheck2Circle size={30} color="red" />
+                    <h1 className="pl-1 text-xl font-semibold">
+                      Customer Centred
+                    </h1>
+                  </div>
+                  <div className="pt-3">
+                    <p className=" w-[90%] font-thin text-lg">
+                      Personalised solutioms delivered by professionals with
+                      trust, transparency, and reliability.
+                    </p>
+                  </div>
+                </div>
+                <div className="w-[80%] py-[3rem]">
+                  <div className="flex items-center">
+                    <BsCheck2Circle size={30} color="red" />
+                    <h1 className="pl-1 text-xl font-semibold">
+                      Global Presence
+                    </h1>
+                  </div>
+                  <div className="pt-3">
+                    <p className=" w-[95%] font-thin text-lg">
+                      Active partnerships and operations worldwide that cater
+                      for international transactions and diverse customer
+                      requirements
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="w-full">
+                <div className=" w-[85%] border-b px-5 border-[#9B9B9B] pb-[1.3rem]">
+                  <div className="flex items-center">
+                    <BsCheck2Circle size={30} color="red" />
+                    <h1 className="pl-1 text-xl font-semibold">
+                      Comprehensive Solutions
+                    </h1>
+                  </div>
+                  <div className="pt-3">
+                    <p className=" w-[90%] font-thin text-lg">
+                      Numerous financial services, including foreign exchange
+                      transactions, import and export services to seamlessly
+                      meet your financial needs.
+                    </p>
+                  </div>
+                </div>
+                <div className=" w-[85%] border-b px-5 border-[#9B9B9B] py-[3rem]">
+                  <div className="flex items-center">
+                    <BsCheck2Circle size={30} color="red" />
+                    <h1 className="pl-1 text-xl font-semibold">
+                      Secure Transactions
+                    </h1>
+                  </div>
+                  <div className="pt-3">
+                    <p className=" font-thin text-lg">
+                      Robust security measures to ensure secure and effective
+                      financial transactions.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full">
+          <div className="py-[9rem] px-[18rem]">
+            <div className="text-center">
+              <h1 className="text-3xl font-semibold">OUR PARTNERS</h1>
+            </div>
+            <div className="w-full pt-5">
+              <div className="flex items-center">
+                <div>
+                  <img className="h-[5rem]" src={Cbn} alt="" />
+                </div>
+                <div>
+                  <img className="h-[5rem]" src={Cbn} alt="" />
+                </div>
+                <div>
+                  <img className="h-[5rem]" src={Cbn} alt="" />
+                </div>
+                <div>
+                  <img className="h-[5rem]" src={Cbn} alt="" />
+                </div>
+                <div>
+                  <img className="h-[5rem]" src={Cbn} alt="" />
+                </div>
+              </div>
+              <div></div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ import Ino from "../../Assets/img/Ino.svg";
 function NavBar() {
   return (
     <div className="w-full fixed z-20">
-      <nav className=" bg-white floated">
+      <nav className="floated active">
         <div className="w-full justify-center flex">
           <img className="h-[7rem] " src={Ino} alt="Ino" />
         </div>
